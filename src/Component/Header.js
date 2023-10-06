e1e516fe8d20f2402ede31e621321ee0fcd74e3b
Anna Makarewicz <annamakarewicz@Annas-MacBook-Air-10.local>
2023-10-06 10:32:34 +0200

    changes

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -7,7 +7,10 @@ function Header()
             <div class="flex-small one-third text-left"><h1>Chillato</h1></div>
             <div class="flex-small one-third text-center"><h1>New Trip From Scratch</h1></div>
             <div class="flex-small one-third text-right"><h1>Explorato</h1></div>
-        </div>
+	    <button>Home</button>
+	    <button>Create</button>
+	    <button>Profile</button>
+	</div>
      );
 }
 
