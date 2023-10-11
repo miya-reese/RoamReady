0b49ed1c167de2e2f0c093cafc086bbc4556ab6a
drewkim <drkim@calpoly.edu>
2023-10-11 18:12:13 +0200

    listview first commit

--- src/Component/SingleTrip.js
+++ src/Component/SingleTrip.js
@@ -24,13 +24,7 @@ function BasicExample() {
           <Accordion.Item eventKey="1">
             <Accordion.Header>Accordion Item #2</Accordion.Header>
             <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
+              LZ
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
@@ -39,13 +33,13 @@ function BasicExample() {
   }
 
 function SingleTrip() {
-    const [name, setName] = useState("");
 return (
 <>
 <h1>
 Your Trip to Amsterdam
 </h1>
 <img src={romeToAmsterdam}/>
+<BasicExample />
 </>
 )
 }
