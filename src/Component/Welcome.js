cae4180431f3da57005425ab466072a078ac30cc
Anna Makarewicz <annamakarewicz@Annas-MacBook-Air-10.local>
2023-10-11 18:09:00 +0200

    fixed logo not showing up on welcome

--- src/Component/Welcome.js
+++ src/Component/Welcome.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './ROAM.png';
+import logo from './images/ROAM.png';
 import './css/Welcome.css'; // Import your CSS file
 
 function Welcome(props) {
     return (
         <div className="center">
-            <img src={logo} width={600} height={600} alt="Logo" />
+            <img src={logo} alt="Logo" className="centered-image" />
         </div>
     );
 }
