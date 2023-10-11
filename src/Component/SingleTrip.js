41d25ba38fc8574038a7fdb6174978c854146af1
drewkim <drkim@calpoly.edu>
2023-10-11 19:40:31 +0200

    second commit

--- src/Component/SingleTrip.js
+++ src/Component/SingleTrip.js
@@ -18,7 +18,7 @@ function BasicExample() {
               aliquip ex ea commodo consequat. Duis aute irure dolor in
               reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
               pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
+              culpa qui officia deserunt mollit anim id est labor
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="1">
@@ -38,7 +38,9 @@ return (
 <h1>
 Your Trip to Amsterdam
 </h1>
-<img src={romeToAmsterdam}/>
+<div className="imgParent">
+<img className= "imgChild" src={romeToAmsterdam} alt="rToA"/>
+</div>
 <BasicExample />
 </>
 )
