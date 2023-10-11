7a55a8b6f1e78a019c67308b6bcbb51233ac66d8
drewkim <drkim@calpoly.edu>
2023-10-12 00:58:33 +0200

    first commit contListView

--- src/Component/SingleTrip.js
+++ src/Component/SingleTrip.js
@@ -1,51 +1,78 @@
 import "./css/singleTrip.css";
 import romeToAmsterdam from "./images/romeToAmsterdam.png";
 import Accordion from "react-bootstrap/Accordion";
-
-import AccordionHeader from "react-bootstrap/AccordionHeader";
-import AccordionBody from "react-bootstrap/AccordionBody";
-import AccordionButton from "react-bootstrap/AccordionButton";
+import Dropdown from 'react-bootstrap/Dropdown';
+import { Link } from "react-router-dom";
 
 console.log(romeToAmsterdam);
 
-function BasicExample() {
+function TripSteps() {
     return (
-        <div className="singleTripAccordion" style={{float: "right"}}>
+        // <div className="singleTripAccordion" style={{float: "right"}}>
         <Accordion defaultActiveKey="0">
           <Accordion.Item eventKey="0">
-            <Accordion.Header>Accordion Item #1</Accordion.Header>
+            <Accordion.Header>Fiumincino Airport to Schipol Amsterdam Airport</Accordion.Header>
             <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est labor
+                <Link to="https://www.expedia.com/">
+                7:30, Turkish Airlines
+                </Link>
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="1">
-            <Accordion.Header>Accordion Item #2</Accordion.Header>
+            <Accordion.Header>Metro to Hotel</Accordion.Header>
             <Accordion.Body>
-              LZ
+              Every 15 minutes
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>3 nights at Hilton Amsterdam</Accordion.Header>
+            <Accordion.Body>
+              October 11th to October 14th
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
+        // </div>
+      );
+}
+
+function FilterByDropDown() {
+    return (
+        <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Filter by:
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Transportation</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Accommodations</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Activities</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
     );
-  }
+}
 
 function SingleTrip() {
 return (
 <>
-<h1>
-Your Trip to Amsterdam
-</h1>
-<div className="imgParent">
-<img className= "imgChild" src={romeToAmsterdam} alt="rToA"/>
-</div>
-<BasicExample />
+    <h1>
+        Your Trip to Amsterdam
+    </h1>
+
+    <div className="box">
+        <div className="imgParent">
+            <img className= "imgChild" src={romeToAmsterdam} alt="rToA"/>
+        </div>
+        <div className="accordionParent">
+            <h1>
+                <FilterByDropDown />
+            </h1>
+            <div className="accordionChild">
+                <TripSteps />
+            </div>
+        </div>
+    </div>
 </>
-)
+);
 }
 
 export default SingleTrip;
