00e2b12f4969c24113411a162c5f62462802b6c6
drewkim <drkim@calpoly.edu>
2023-10-11 09:27:53 +0200

    first accordion

--- src/Component/SingleTrip.js
+++ src/Component/SingleTrip.js
@@ -2,34 +2,25 @@ import "./css/singleTrip.css";
 import romeToAmsterdam from "./images/romeToAmsterdam.png";
 import Accordion from "react-bootstrap/Accordion";
 
+import AccordionHeader from "react-bootstrap/AccordionHeader";
+import AccordionBody from "react-bootstrap/AccordionBody";
+import AccordionButton from "react-bootstrap/AccordionButton";
 
 console.log(romeToAmsterdam);
 
 function BasicExample() {
     return (
-        <div className="singleTripAccordion" style={{float: "right"}}>
-        <Accordion defaultActiveKey="0">
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>Accordion Item #1</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="1">
-            <Accordion.Header>Accordion Item #2</Accordion.Header>
-            <Accordion.Body>
-              LZ
-            </Accordion.Body>
-          </Accordion.Item>
+        <Accordion>
+            <AccordionHeader>
+                <AccordionButton>
+                    step 1
+                </AccordionButton>
+            </AccordionHeader>
+            <AccordionBody>
+                go to fiumincino airport
+            </AccordionBody>
         </Accordion>
-        </div>
-      );
+    );
   }
 
 function SingleTrip() {
@@ -38,7 +29,10 @@ return (
 <h1>
 Your Trip to Amsterdam
 </h1>
-<img src={romeToAmsterdam}/>
+<img src={romeToAmsterdam} alt="rToA"/>
+<p>
+    1. take plane
+</p>
 <BasicExample />
 </>
 )
