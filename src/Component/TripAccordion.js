90ed1e45b43fbfd763a8df92dd76f23f6b4c65fd
drewkim <drkim@calpoly.edu>
2023-10-15 17:48:25 +0200

    filterIDs first commit

--- src/Component/TripAccordion.js
+++ src/Component/TripAccordion.js
@@ -1,12 +1,19 @@
 import Accordion from "react-bootstrap/Accordion";
 
-const TripAccordion = ({title, content, transportColor, accoColor, actColor}) => {
+const TripAccordion = ({title, content, type, transportColor, accoColor, actColor}) => {
+    let color;
+    if(type == 0)
+        color = transportColor;
+    else if(type == 1)
+        color = accoColor;
+    else
+        color = actColor;
     return (
         <>
             <Accordion>
                 <Accordion.Item eventKey="0" class="testTrip">
                     <Accordion.Header>
-                        <body style={{color: transportColor}}>
+                        <body style={{color: color}}>
                         {title}
                         </body>
                     </Accordion.Header>
