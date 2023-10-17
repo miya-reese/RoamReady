dd49f831cfeed21c4f8303e881ad1b8b787eab35
soren-fliegel <83089360+soren-fliegel@users.noreply.github.com>
2023-10-17 10:21:17 +0000

    completed func, only for future trips

--- src/Component/FormController.js
+++ src/Component/FormController.js
@@ -2,13 +2,12 @@ import MapObject from "./classes/MapObject";
 import ItineraryObject from "./classes/ItineraryObject";
 import TripObject from "./classes/TripObject";
 import TripArray from "../TripArray";
-import TripCardInfo from "./MyTrips";
 
 function FormController(formData)
 {
     let userMap = new MapObject(formData.location1, formData.location2);
     let userItinerary = new ItineraryObject(formData.startDate, formData.endDate);
-    let userTrip = new TripObject(formData.tripName, userMap, userItinerary, formData.budget, formData.numTravelers);
+    let userTrip = new TripObject(formData.tripName, userMap, userItinerary, formData.budget, formData.numTravelers, '');
 
     console.log(userTrip.name);
     console.log(userTrip.MapObject.locationList[0]);
@@ -19,13 +18,6 @@ function FormController(formData)
     console.log(userTrip.travelers);
 
     TripArray.push(userTrip);
-
-    let cardInfo = new TripCardInfo(userTrip.name, userTrip.ItineraryObject.startDate+" - "+userTrip.ItineraryObject.endDate, '');
-    console.log("HERE: ");
-    console.log(userTrip.ItineraryObject.startDate+"-"+userTrip.ItineraryObject.endDate);
-    //TripArray.push(cardInfo);
-    console.log(cardInfo.name);
-    console.log(cardInfo.name);
 }
 
 export default FormController;
--- src/Component/MyTrips.js
+++ src/Component/MyTrips.js
@@ -2,37 +2,25 @@ import React from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import TripCard from './TripCard.js'
-import TripCardInfo from './classes/TripCardInfo.js';
 import TripArray from '../TripArray.js';
-
-// trip arrays
-// const upcomingTrips = [];
-// const pastTrips = [];
-
-// add trip to arrays
-let romeTrip = new TripCardInfo('Clubbing in Roma', '10/13/2023 - 10/14/2023', 'https://www.wantedinrome.com/i/preview/storage/uploads/2019/03/Clubs_in_Rome.jpg');
-TripArray.push(romeTrip);
-TripArray.push(new TripCardInfo("A weekend in Florence", "10/20/2023 - 10/22/2023", "https://media.timeout.com/images/105879414/750/422/image.jpg"));
-
-// pastTrips.push(new TripCardInfo('Amalfi Coast', '10/06/2023 - 10/08/2023', "https://www.tourissimo.travel/hubfs/Blog_pictures/The%20Path%20of%20Gods/Path%20of%20the%20Gods%20Blog%20Banner.jpg"),
-// new TripCardInfo("Cinque Terre", '09/29/2023 - 10/01/2023', "https://lp-cms-production.imgix.net/2021-08/GettyRF_595500583.jpg?auto=format&q=75&w=1920"),
-// new TripCardInfo('Oktoberfest!', '09/22/2023 - 09/25/2023', 'https://static2-viaggi.corriereobjects.it/wp-content/uploads/2023/09/schuetzen_festzelt_architektur_sebastian_lehner-9665.jpg?v=1694707947'));
 
 // the page
 function MyTripsPage() {
 
   // map array into node
   let displayUpcomingTrips = <h3 class="text-center">No trips planned, get on it!!! :P</h3>;
-  displayUpcomingTrips = TripArray.map(trip => 
-    <Col><TripCard name={trip.name} date={trip.date} srcImg={trip.srcImg}></TripCard></Col>
-    );
+  // displayUpcomingTrips = TripArray.map(trip => 
+  //   <Col><TripCard name={trip.name} date={trip.startDate+"-"+trip.endDate} srcImg={trip.srcImg}></TripCard></Col>
+  //   );
   if (TripArray.length > 0)
   {
     displayUpcomingTrips = TripArray.map(trip => 
-      <Col><TripCard name={trip.name} date={trip.date} srcImg={trip.srcImg}></TripCard></Col>
+      <Col><TripCard name={trip.name} date={trip.ItineraryObject.startDate+' - '+trip.ItineraryObject.endDate} srcImg={trip.srcImg}></TripCard></Col>
       );
   }
   
+  //const pastTrips = [];
+
   // let displayPastTrips = <h3 class="text-center">You haven't gone on any trips... yet :D </h3>;
   // if (pastTrips.length > 0) {
   //   displayPastTrips = pastTrips.map(trip => 
