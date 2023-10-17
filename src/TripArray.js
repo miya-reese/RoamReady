3240cda8a38d9fd6155ce4a09c1318c0bbc14e74
soren-fliegel <83089360+soren-fliegel@users.noreply.github.com>
2023-10-17 16:34:46 +0000

    ordered hard coded trips

--- src/TripArray.js
+++ src/TripArray.js
@@ -5,15 +5,15 @@ import TripObject from "./Classes/TripObject";
 const TripArray = [];
 
 /****** 5 Hard Coded Example Trips, 2 future, 3 past ******/
+let florenceMap = new MapObject('','');
+let florenceItinerary = new ItineraryObject(new Date('November 20, 2023'), new Date('November 22, 2023'));
+let florenceTrip = new TripObject('Florence Trip', florenceMap, florenceItinerary, 0, 0, 'https://media.timeout.com/images/105879414/750/422/image.jpg');
+TripArray.push(florenceTrip);
+
 let romeMap = new MapObject('','');
 let romeItinerary = new ItineraryObject(new Date('November 13, 2023'), new Date('November 14, 2023'));
 let romeTrip = new TripObject('Clubbing in Roma', romeMap, romeItinerary, 0, 0, 'https://www.wantedinrome.com/i/preview/storage/uploads/2019/03/Clubs_in_Rome.jpg');
 TripArray.push(romeTrip);
-
-let florenceMap = new MapObject('','');
-let florenceItinerary = new ItineraryObject(new Date('November 20, 2023'), new Date('November 22, 2023'));
-let florenceTrip = new TripObject('Clubbing in Roma', florenceMap, florenceItinerary, 0, 0, 'https://media.timeout.com/images/105879414/750/422/image.jpg');
-TripArray.push(florenceTrip);
 
 let amalfiMap = new MapObject('','');
 let amalfiItinerary = new ItineraryObject(new Date('October 6, 2023'), new Date('Octobere 8, 2023'));
