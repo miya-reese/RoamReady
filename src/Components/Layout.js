25b83ff05beee49ecd69963a36d1a510a687e7c9
soren-fliegel <83089360+soren-fliegel@users.noreply.github.com>
2023-10-27 10:02:25 +0000

    some edits to navbar, not done

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,42 +1,43 @@
 import { Outlet, Link } from "react-router-dom";
-//import styles from './css/Navbar.module.css';
+import styles from './css/Navbar.module.css';
 // import logo from './images/ROAM.png';
 
 
 const Layout = () => {
   return (
-    <div className="App">
-        <header className="App-header">
-            {/*className={`${styles.navbar}`}*/}
-            <nav className='p-1 m-0' style={{background:'#5f9ea0'}}>
+    <div className="App" >
+        {/* <header className="App-header"> */}
+        {/*className={`${styles.navbar}`}*/}
+        {/* style={{background:'#5f9ea0', border: '2px solid red'}} */}
+        <nav className={`${styles.navbar}`}>
 
-                {/* logo */}
-                <Link to="/Welcome">
-                        <h1 style={{fontFamily:'League Spartan', color:'white'}}>RoamReady</h1>
-                        {/* <img src={logo} alt="logo" style={{width: 200, height: 200}}/> */}
-		        </Link>
-                {/* className={`${styles.navMenu}` */}
-                <ul>
-                    <li>
-                        <Link to="/Card">About Us</Link>
-                    </li>
-                        
-                    <li>
-                        <Link to="/MyTrips">My Trips</Link>                    
-                    </li>
+            {/* logo */}
+            <Link className='custom-link' to="/Welcome">
+                    <h1>RoamReady</h1>
+                    {/* <img src={logo} alt="logo" style={{width: 200, height: 200}}/> */}
+            </Link>
+            {/* className={`${styles.navMenu}` */}
+            <ul className={`${styles.navMenu}`}>
+                <li>
+                    <Link to="/Card">About Us</Link>
+                </li>
+                    
+                <li>
+                    <Link to="/MyTrips">My Trips</Link>                    
+                </li>
 
-                    <li>
-                        <Link to="/FormScreen">Create</Link>
-                    </li>
+                <li>
+                    <Link to="/FormScreen">Create</Link>
+                </li>
 
-                    <li>
-                        <Link to="/Card">Profile</Link>
-                    </li>
-                </ul>
-            </nav>
+                <li>
+                    <Link to="/Card">Profile</Link>
+                </li>
+            </ul>
+        </nav>
 
-            <Outlet />
-        </header>
+        <Outlet />
+        {/* </header> */}
     </div>
     );
 }
