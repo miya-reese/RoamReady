90468a6d5239acc70c676ddcf943d4c116e901c1
drewkim <drkim@calpoly.edu>
2023-10-27 11:06:20 +0200

    trip name grammar

--- src/Components/MyTrips.js
+++ src/Components/MyTrips.js
@@ -13,9 +13,9 @@ function MyTripsPage()
   let displayPastTrips = <h3 className="text-center">You haven't gone on any trips... yet :D </h3>;
   if (PastTrips.length > 0) {
     displayPastTrips = PastTrips.map(trip => 
-      <div key={trip.name}>
-        <Col><TripCard name={trip.name} date={(trip.ItineraryObject.startDate.toDateString()).slice(4)+' - '+(trip.ItineraryObject.endDate.toDateString()).slice(4)} srcImg={trip.srcImg}></TripCard></Col>
-      </div>
+      <Col>
+        <div key={trip.name}><TripCard name={trip.name} date={(trip.ItineraryObject.startDate.toDateString()).slice(4)+' - '+(trip.ItineraryObject.endDate.toDateString()).slice(4)} srcImg={trip.srcImg}></TripCard></div>
+      </Col>
       );
   }
 
@@ -23,14 +23,14 @@ function MyTripsPage()
   let displayUpcomingTrips = <h3 className="text-center">No trips planned, get on it!!! :P</h3>;
   if (TripArray.length > 0) {
     displayUpcomingTrips = TripArray.map(trip => 
-      <div key={trip.name}>
-        <Col><TripCard {...trip} date={(trip.ItineraryObject.startDate.toDateString()).slice(4)+' - '+(trip.ItineraryObject.endDate.toDateString()).slice(4)} srcImg={trip.srcImg}></TripCard></Col>
-      </div>
+      <Col>
+        <div key={trip.name}><TripCard {...trip} name={trip.name} date={(trip.ItineraryObject.startDate.toDateString()).slice(4)+' - '+(trip.ItineraryObject.endDate.toDateString()).slice(4)} srcImg={trip.srcImg}></TripCard></div>
+      </Col>
       );
   }
   
   // display on page
-  return (
+  return (  
     <div>
       <Row>
         <div style={{ height: '1vh'}} ></div>
