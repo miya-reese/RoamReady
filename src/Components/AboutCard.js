98e24948c497403f848c33c09899f536f42e4cdb
soren-fliegel <83089360+soren-fliegel@users.noreply.github.com>
2023-11-15 10:37:27 +0000

    aboutus page finished but missing image

--- src/Components/AboutCard.js
+++ src/Components/AboutCard.js
@@ -2,10 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types';
 //import './css/UI.css';
 
+const imageStyle = {
+  width: '280px',
+  height: '280px',
+  objectFit: 'cover', // Prevents the image from stretching
+};
+
   function AboutCard(props) {
     return (
       <div style={{margin:5}}>
-        <img src={props.srcImg} width={280} height={280} alt={props.name}></img>
+        <img src={props.srcImg} style={imageStyle} alt={props.name}></img>
         <div className='ABCard-div'>
           <h1 className='card-title'>{props.person.name}</h1>
           <h2 className='ABcard-date'>{props.person.description}</h2>
