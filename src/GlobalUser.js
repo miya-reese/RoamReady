583a246c67f456ab47bd0197eecc47ffd01eae6e
miya-reese <miya.reese01@gmail.com>
2023-11-21 01:19:07 +0100

    manage profile backend

---
+++ src/GlobalUser.js
@@ -0,0 +1,6 @@
+import User from './Classes/UserObject.js';
+
+const mainUser = new User("Francisco Lachowski", "chico.lachowski", "milo123", "+1 888 123-4567", "chico_lachowski@gmail.com", "525 E 86th Street", "New York, NY 10651-78")
+mainUser.setProfileImg('https://pbs.twimg.com/media/DUxovauUMAEgKVv.jpg')
+
+export default mainUser;
