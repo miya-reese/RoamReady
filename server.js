f58fd2fbec180fb0dcb356713681bfe5f65eeb63
Anna Makarewicz <annamakarewicz@Annas-Air-10.homenet.telecomitalia.it>
2023-11-21 22:55:38 +0100

    fixed security vulnerability i hope

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+app.disable("x-powered-by");
 const path = require('path');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
