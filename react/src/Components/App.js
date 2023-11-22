e31d99d08c93f97b209ab36ea07705d35d654daf
Anna Makarewicz <annamakarewicz@Annas-MacBook-Air-10.local>
2023-11-22 11:44:30 +0100

    consolidated app.js and added images

--- react/src/Components/App.js
+++ react/src/Components/App.js
@@ -8,26 +8,30 @@ import Card from './Card';
 import MyTrips from './MyTrips';
 import SingleTrip from './SingleTrip';
 import FormScreen from './FormScreen';
+import ManageProfile from './ManageProfile';
+import AboutUsPage from './AboutUs';
+import AddItemFormScreen from './AddItem/AddItemFormScreen';
 import Login from './Login.js';
 import Register from './Register.js';
-
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Layout />}>
-                          <Route index element={<Welcome />} />
-                          <Route path="Welcome" element={<Welcome />} />
-                          <Route path="FormScreen" element={<FormScreen />} />
-                          <Route path="Card" element={<Card />} />
-                          <Route path="MyTrips" element={<MyTrips />}>
-                            <Route index element={<NoPage />} />
-                            <Route path="SingleTrip" element={<SingleTrip />} />
-                          </Route>
-                          <Route path="Register" element={<Register />} />
-                          <Route path="Login" element={<Login />} />
-                        </Route>
+                    <Route index element={<Welcome />} />
+                    <Route path="Welcome" element={<Welcome />} />
+                    <Route path="FormScreen" element={<FormScreen />} />
+                    <Route path="Profile" element={<Card />} />
+                    <Route path="Profile/ManageProfile" element={<ManageProfile/>}/>
+                    <Route path="*" element={<NoPage />}/>
+                    <Route path="MyTrips" element={<MyTrips />}/>
+                    <Route path="MyTrips/SingleTrip" element={<SingleTrip />}/>
+                    <Route path="AboutUs" element={<AboutUsPage />}/>
+                    <Route path="MyTrips/SingleTrip/AddItem" element={<AddItemFormScreen/>}/>
+                    <Route path="Register" element={<Register />} />
+                    <Route path="Login" element={<Login />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
